8485bd2c49509dcff6c6611505307e59f99d9642
MD SAIFULLAH <madinaeshop18@gmail.com>
2023-10-01 03:29:22 -0700

    freagment ok code in thhis time alhamdulillah

--- app/src/main/java/com/madinafinal/madinaeshop/Freagment/HomeFragment.kt
+++ app/src/main/java/com/madinafinal/madinaeshop/Freagment/HomeFragment.kt
@@ -5,9 +5,16 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.denzcoskun.imageslider.constants.ScaleTypes
+import com.denzcoskun.imageslider.models.SlideModel
 import com.madinafinal.madinaeshop.R
+import com.madinafinal.madinaeshop.databinding.FragmentHomeBinding
+import kotlin.collections.ArrayList
+
 
 class HomeFragment : Fragment() {
+    private lateinit var binding: FragmentHomeBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
@@ -20,8 +27,26 @@ class HomeFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        binding = FragmentHomeBinding.inflate(inflater, container,false)
+        return binding.root
+
+
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
+
+        val imageList = ArrayList<SlideModel>()
+        imageList.add(SlideModel(R.drawable.madina_banner1, ScaleTypes.FIT))
+        imageList.add(SlideModel(R.drawable.jaytun_banner2, ScaleTypes.FIT))
+        imageList.add(SlideModel(R.drawable.dryfood_banner, ScaleTypes.FIT))
+        imageList.add(SlideModel(R.drawable.khalisa_banner, ScaleTypes.FIT))
+        imageList.add(SlideModel(R.drawable.sorisamodhu_banner, ScaleTypes.FIT))
+
+
+        val imageSlider = binding.imageSlider
+        imageSlider.setImageList(imageList)
+        imageSlider.setImageList(imageList, ScaleTypes.FIT)
+    }
 }
